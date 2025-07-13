16d68676f755f438063a909de757d899e1cad2eb
Akash Mourya <akashmourya9522@gmail.com>
2025-07-13 23:15:38 +0530

    Renders the Suggested Doctors For A Request

--- app/(routes)/dashboard/_components/AddNewSession.tsx
+++ app/(routes)/dashboard/_components/AddNewSession.tsx
@@ -1,4 +1,4 @@
-"use client"
+"use client";
 import { Button } from "@/components/ui/button";
 import {
   Dialog,
@@ -11,15 +11,37 @@ import {
 } from "@/components/ui/dialog";
 import { Textarea } from "@/components/ui/textarea";
 import { DialogClose } from "@radix-ui/react-dialog";
-import { ArrowRight } from "lucide-react";
+import { IconArrowBadgeRight } from "@tabler/icons-react";
+import axios from "axios";
+import { ArrowRight, Loader, Loader2 } from "lucide-react";
 
 import React, { useState } from "react";
 
 function AddNewSession() {
-    const[note,setNote] = useState<string>()
-    function handleTextArea(e: React.ChangeEvent<HTMLTextAreaElement>) {
-        setNote(e.target.value)
+  const [note, setNote] = useState<string>();
+  const [loading, setLoading] = useState<boolean>(false);
+  const [suggestDoctors, setSuggestDoctors] = useState([]);
+
+  function handleTextArea(e: React.ChangeEvent<HTMLTextAreaElement>) {
+    setNote(e.target.value);
+  }
+  console.log(suggestDoctors);
+
+  async function handleUserReq() {
+    try {
+      setLoading(true);
+      const response = await axios.post("/api/suggest-doctors", { note });
+      console.log(response.data);
+      setSuggestDoctors(response.data);
+      setLoading(false);
+    } catch (error) {
+      console.log(
+        "There seems to be an error while sending the request to /api/suggest-doctors",
+        error
+      );
     }
+  }
+
   return (
     <Dialog>
       <DialogTrigger>
@@ -29,21 +51,37 @@ function AddNewSession() {
         <DialogHeader>
           <DialogTitle>Add Details Regarding Your Illness</DialogTitle>
           <DialogDescription asChild>
-            <div className="mt-5 ">
-              <Textarea
-              onChange={handleTextArea}
-                className="h-[150px]"
-                placeholder="Enter How Do You Feel Right Now!"
-              />
-            </div>
+            {suggestDoctors.length > 0 ? (
+              suggestDoctors.map((doctor) => (
+                <div key={doctor.id}>
+                  <h2 className="font-bold mt-2">{doctor.specialist}</h2>
+                  <h2 className="text-gray-500 line-clamp-2 mt-2">
+                    {doctor.reason}
+                  </h2>
+                  <Button className="mt-3">
+                    Consult <IconArrowBadgeRight />{" "}
+                  </Button>
+                </div>
+              ))
+            ) : (
+              <div className="mt-5 ">
+                <Textarea
+                  onChange={handleTextArea}
+                  className="h-[150px]"
+                  placeholder="Enter How Do You Feel Right Now!"
+                />
+              </div>
+            )}
           </DialogDescription>
         </DialogHeader>
         <DialogFooter>
           <DialogClose>
             <Button variant={"outline"}>Cancel</Button>
           </DialogClose>
-          <Button disabled={!note}>
-            Recommed A Doctor <ArrowRight />
+          
+          <Button onClick={handleUserReq} disabled={!note || loading}>
+            
+            {loading && <Loader2 className="animate-spin"></Loader2> } Recommed A Doctor <ArrowRight />
           </Button>
         </DialogFooter>
       </DialogContent>
--- app/api/suggest-doctors/route.ts
+++ app/api/suggest-doctors/route.ts
@@ -1,32 +1,35 @@
-import { db } from "@/config/db";
 import { openai } from "@/config/openAiModel";
-import { usersTable } from "@/config/schema";
 import { AIDoctorAgents } from "@/doctorList/list";
-import { currentUser } from "@clerk/nextjs/server";
-import { eq } from "drizzle-orm";
 import { NextRequest, NextResponse } from "next/server";
 
 export async function POST(req: NextRequest) {
-    console.log("Hello from the post request")
-    const {notes} = await req.json()
-    console.log(notes)
+  const { notes } = await req.json();
   try {
     const completion = await openai.chat.completions.create({
       model: "google/gemini-2.0-flash-exp:free",
       messages: [
         {
-            role:"system",
-            content:JSON.stringify(AIDoctorAgents)
+          role: "system",
+          content: JSON.stringify(AIDoctorAgents),
         },
         {
           role: "user",
-          content: "User notes/symptoms"+notes+"depends on user notes and symptoms, please suggest list of doctors . Return object in JSON only",
+          content:
+            "User notes/symptoms" +
+            notes +
+            "depends on user notes and symptoms, please suggest list of doctors . Return object in JSON only",
         },
       ],
     });
-    const gptResponse = (completion.choices[0].message);
-    return NextResponse.json(gptResponse)
+    const gptResponse = completion.choices[0].message;
+    const cleanedResponse = gptResponse.content
+      ?.trim()
+      .replace("```json", "")
+      .replace("```", "");
+
+    const stringVersion = JSON.parse(cleanedResponse);
+    return NextResponse.json(stringVersion);
   } catch (error) {
-    return NextResponse.json(error)
+    return NextResponse.json(error);
   }
 }
